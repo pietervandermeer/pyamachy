a200d93ddadf7939651064a1b4b293889a9969bb
Pieter van der Meer <pieter.van.der.meer@sron.nl>
2014-09-14 21:16:43 +0200

    added unfinished test if simudark corrects everything the right way..

--- plot_extracted_states.py
+++ plot_extracted_states.py
@@ -27,6 +27,7 @@ import numpy
 from numpy import arange, sin, pi
 import h5py
 import ctypes as ct
+from sciamachy_module import NonlinCorrector
 
 # Used to guarantee to use at least Wx2.8
 import wxversion
@@ -37,13 +38,53 @@ import wxversion
 # comment out the following to use wx rather than wxagg
 matplotlib.use('WXAgg')
 
+# read simudark data for specified orbit (only ch8 data!)
+def read_simudark(orbit, ao=None, lc=None, amp1=None):
+    dict = {}
+
+    #
+    # obtain indices to entries of the requested orbit
+    #
+
+    fid = h5py.File("/SCIA/share/SDMF/3.1/sdmf_simudark.h5", 'r') # generates an exception
+    gid = fid["ch8"] # generates an exception
+    mt_did = gid['metaTable']
+    orbitlist = (gid['orbitList'])[:]
+    if orbit not in orbitlist:
+        print('read_simudark(): orbit not present in database')
+        dict['status'] = -1
+        return dict
+
+    metaindx = numpy.where(orbitlist == orbit)
+    metaindx = numpy.asscalar(numpy.array(metaindx))
+    print(metaindx)
+
+    mt_did = gid['metaTable']
+    mtbl = mt_did[metaindx]
+    dict['mtbl'] = mtbl
+
+    if ao:
+        ds_did = gid['ao']
+        dict['ao'] = ds_did[:,metaindx]
+
+    if lc:
+        ds_did = gid['lc']
+        dict['lc'] = ds_did[:,metaindx]
+
+    if amp1:
+        ds_did = gid['amp1']
+        dict['amp1'] = ds_did[:,metaindx]
+
+    dict['status'] = 0
+    return(dict)    
 
 # reads extracted state executions from database
-def read_extracted_states(orbitrange, state_id, calib_db, readoutMean=False, readoutNoise=False, orbitList=False):
+def read_extracted_states(orbitrange, state_id, calib_db, in_orbitlist=None, readoutMean=False, readoutNoise=False, orbitList=False):
 
-    if len(orbitrange) is not 2:
-        print('read_extracted_states: orbitrange should have 2 elements')
-        return
+    if in_orbitlist is None:
+        if len(orbitrange) is not 2:
+            print('read_extracted_states: orbitrange should have 2 elements')
+            return
 
     dict = {}
 
@@ -55,10 +96,14 @@ def read_extracted_states(orbitrange, state_id, calib_db, readoutMean=False, rea
     gid = fid["State_"+str('%02d'%state_id)] # generates an exception
     mt_did = gid['metaTable']
     orbitlist = (gid['orbitList'])[:]
-    print('orbitrange=', orbitrange)
-#    metaindx = numpy.where((orbitlist >= orbitrange[0]) & (orbitlist <= orbitrange[1]))
-    metaindx = (orbitlist >= orbitrange[0]) & (orbitlist <= orbitrange[1])
-    #print(metaindx)
+    if in_orbitlist is not None:
+        #metaindx = orbitlist = in_orbitlist
+        metaindx = numpy.in1d(orbitlist, in_orbitlist)
+    else:
+        print('orbitrange=', orbitrange)
+        metaindx = (orbitlist >= orbitrange[0]) & (orbitlist <= orbitrange[1])
+        print(metaindx)
+
     if metaindx[0].size is 0:
         print('read_extracted_states: orbit range not present in database')
         dict['status'] = -1
@@ -67,7 +112,7 @@ def read_extracted_states(orbitrange, state_id, calib_db, readoutMean=False, rea
     mtbl = mt_did[metaindx]
     dict['mtbl'] = mtbl
 
-    if readoutNoise:
+    if readoutMean:
         ds_did      = gid['readoutMean']
         dict['readoutMean'] = ds_did[metaindx,:]
 
@@ -83,86 +128,131 @@ def read_extracted_states(orbitrange, state_id, calib_db, readoutMean=False, rea
     dict['status'] = 0
     return(dict)
 
+# test function to check old simudark quality
+def check_eclipse_calib():
+    orbit = 24000
+
+    #
+    # get state executions and correct them for nonlinearity
+    # 
+
+    db_name = "/SCIA/SDMF31/sdmf_extract_calib.h5"
+    states = read_extracted_states([orbit,orbit], 8, db_name, readoutMean=True)
+    print(states['readoutMean'].shape)
+    readouts = states['readoutMean'] #states['readoutMean'][:,7*1024:8*1024]
+    print(readouts.shape)
+    nlc = NonlinCorrector()
+    for idx_exec in range(readouts.shape[0]):
+        readouts[idx_exec,:] = nlc.correct(readouts[idx_exec,:])
+
+    #
+    # correct for dark current with simudark v1
+    #
+
+    petcor = 1.18125e-3
+    simudark = read_simudark(orbit, ao=True, lc=True, amp1=True)
+    mtbl = simudark['mtbl']
+    phases = mtbl['orbitPhase'][:]
+    readouts_ch8 = readouts[:,7*1024:8*1024]
+    readouts_ch8 -= simudark['ao']
+    readouts_ch8 -= simudark['lc'] * (1-petcorr) # TODO: PET belonging to the state data
+    for idx_exec in range(readouts.shape[0]):
+        
+    readouts_ch8 -= 
+    print(readouts_ch8.shape)
+    return
+
+# plot dark state executions (detrend and overplot them)
+def check_darkstates():
+    testlib = ct.CDLL('testlib.so')
+
+    #testlib.myprint()
+    #d = ct.c_double(1.234)
+    #ds = (ct.c_double * 4)()
+    #for i in range(4):
+    #    ds[i] = i
+    #testlib.print_double(d)
+    #testlib.print_doubles(ds, ct.c_long(len(ds)))
+
+    #n_elem = 4
+    #zs = numpy.arange(n_elem)
+    #print(zs)
+    #zs = zs.astype(numpy.float64)
+    #testlib.print_doubles(zs.ctypes.data_as(ct.POINTER(ct.c_double)), ct.c_long(n_elem))
+
+    db_name = "/SCIA/SDMF31/sdmf_extract_calib.h5"
+    orbit_range = [24000,24005]
+    pix = 7*1024+140 
+
+    n_orbits = orbit_range[1] - orbit_range[0]
+
+    in_orbitlist =  [20237,20238,20666,20667,21081,21082,21511,21512,21954,21955,22384,22385,22799,22800,23214,23215,23629,23630,24044,24045,24474,24475,24889,24890,25318,25319,25733,25734,26163,26164,26563,26564,27022,27023,27437,27438,28052,28053,28295,28296]
+
+    #data = read_extracted_states(orbit_range, 8, db_name, readoutMean=True, readoutNoise=True, orbitList=True)
+    data = read_extracted_states(orbit_range, 8, db_name, in_orbitlist=in_orbitlist, readoutMean=True, readoutNoise=True, orbitList=True)
+
+    #print(data)
+
+    # orbits, pixels, states
+    print(data['readoutMean'].shape)
+
+    #hor_axis = numpy.arange(orbit_range[0], orbit_range[1])
+    #idx = numpy.nonzero(vals)
+    #print(vals)
+
+    colors = ['b','g','r','c','m','y','k']
+
+    #print(data['mtbl'])
+    orbits = data['orbitList']
+    print(orbits)
+    uorbits = numpy.unique(orbits)
+    readoutMean = data['readoutMean']
+    mtbl = data['mtbl']
+    n_records = mtbl.size
+    jds = mtbl['julianDay'][:]
+
+    #
+    # get eclipse orbit phases instead of the SDMF 3.1 polar-based ones
+    #
+
+    saa_flag = False;
+    dummy_orbit = 0
+    # prepare orbit phase output array for c
+    e_phases = numpy.zeros(n_records) # TODO: directly specify dtype=float64 or so?
+    e_phases = e_phases.astype(numpy.float32)
+    e_phases_c = e_phases.ctypes.data_as(ct.POINTER(ct.c_float))
+    # prepare julianday input array for c
+    jds = jds.astype(numpy.float64)
+    jds_c = jds.ctypes.data_as(ct.POINTER(ct.c_double))
+    testlib._GET_SCIA_ROE_ORBITPHASE(ct.c_bool(True), ct.c_bool(saa_flag), ct.c_long(n_records), ct.c_long(dummy_orbit), e_phases_c, jds_c) 
+
+    print(e_phases)
+
+    phases = mtbl['orbitPhase'][:]
+
+    for i in range(uorbits.size):
+        #print(i, uorbits[i])
+        idx = numpy.where(orbits == uorbits[i])
+        vals = readoutMean[idx, pix]
+        #print(vals)
+        vals = vals.flatten()
+        #hor_axis = phases[idx] #numpy.arange(vals.size)
+        hor_axis = e_phases[idx] #numpy.arange(vals.size)
+        #print(vals.shape)
+        #print(vals)
+        #print(hor_axis.shape)
+        #print(hor_axis)
+        plt.scatter(hor_axis, vals-numpy.mean(vals), color=colors[i%7])
+        #plt.scatter(hor_axis, vals, color=colors[i%7])
+     
+    #plt.savefig('test.png', dpi=100)
+    plt.show()
+    return
+
 #
 # main
 #
 
-testlib = ct.CDLL('testlib.so')
+check_eclipse_calib()
+#check_darkstates()
 
-#testlib.myprint()
-#d = ct.c_double(1.234)
-#ds = (ct.c_double * 4)()
-#for i in range(4):
-#    ds[i] = i
-#testlib.print_double(d)
-#testlib.print_doubles(ds, ct.c_long(len(ds)))
-
-n_elem = 4
-zs = numpy.arange(n_elem)
-#print(zs)
-zs = zs.astype(numpy.float64)
-testlib.print_doubles(zs.ctypes.data_as(ct.POINTER(ct.c_double)), ct.c_long(n_elem))
-
-db_name = "/SCIA/SDMF31/sdmf_extract_calib.h5"
-orbit_range = [24000,25000]
-pix = 7*1024+140 
-
-n_orbits = orbit_range[1] - orbit_range[0]
-
-data = read_extracted_states(orbit_range, 26, db_name, readoutMean=True, readoutNoise=True, orbitList=True)
-
-#print(data)
-
-# orbits, pixels, states
-print(data['readoutMean'].shape)
-
-#hor_axis = numpy.arange(orbit_range[0], orbit_range[1])
-#idx = numpy.nonzero(vals)
-#print(vals)
-
-colors = ['b','g','r','c','m','y','k','w']
-
-#print(data['mtbl'])
-orbits = data['orbitList']
-print(orbits)
-uorbits = numpy.unique(orbits)
-readoutMean = data['readoutMean']
-mtbl = data['mtbl']
-n_records = mtbl.size
-jds = mtbl['julianDay'][:]
-
-#
-# get eclipse orbit phases instead of the SDMF 3.1 polar-based ones
-#
-
-saa_flag = False;
-dummy_orbit = 0
-# prepare orbit phase output array for c
-e_phases = numpy.zeros(n_records) # TODO: directly specify dtype=float64 or so?
-e_phases = e_phases.astype(numpy.float32)
-e_phases_c = e_phases.ctypes.data_as(ct.POINTER(ct.c_float))
-# prepare julianday input array for c
-jds = jds.astype(numpy.float64)
-jds_c = jds.ctypes.data_as(ct.POINTER(ct.c_double))
-testlib._GET_SCIA_ROE_ORBITPHASE(ct.c_bool(True), ct.c_bool(saa_flag), ct.c_long(n_records), ct.c_long(dummy_orbit), e_phases_c, jds_c) 
-
-print(e_phases)
-
-phases = mtbl['orbitPhase'][:]
-
-for i in range(uorbits.size):
-    #print(i, uorbits[i])
-    idx = numpy.where(orbits == uorbits[i])
-    vals = readoutMean[idx, pix]
-    #print(vals)
-    vals = vals.flatten()
-    #hor_axis = phases[idx] #numpy.arange(vals.size)
-    hor_axis = e_phases[idx] #numpy.arange(vals.size)
-    #print(vals.shape)
-    #print(vals)
-    #print(hor_axis.shape)
-    #print(hor_axis)
-    plt.scatter(hor_axis, vals, color=colors[i%8])
- 
-#plt.savefig('test.png', dpi=100)
-plt.show()
