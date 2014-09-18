222effa39571b935e0eec7a8a9f89e28f79c4012
Pieter van der Meer <pieter.van.der.meer@sron.nl>
2014-09-18 15:25:12 +0200

    ongoing work on using the kmpfit package.

--- test_fit_simudark.py
+++ test_fit_simudark.py
@@ -22,51 +22,51 @@ from __future__ import print_function
 import numpy
 from numpy import cos, pi
 from scipy.optimize import curve_fit
+from kapteyn import kmpfit
 
 from sciamachy_module import NonlinCorrector, read_extracted_states
 from simudark_module import simudark_orbvar_function_
 
+
 #- functions -------------------------------------------------------------------
 
-def scia_dark_fun(x, *a):
+def scia_dark_fun(p, x):
     """
      Procedure scia_dark_fun
 
      Purpose:
-       parametric description of dark signal given a
-       number of input parameters:
+       parametric description of dark signal given a number of input parameters:
        * analog offset
        * pixel exposure time
        * co-adding factor
        * dark current
        * orbital phase
        * parameters describing orbital variation
-       * parameters describing non-linear response (not implemented)
+       * parameters describing orbit-to-orbit trend
        Intended to be used with CURVEFIT routine or equivalent
 
      Usage:
-     scia_dark_fun, x, a, f
+     f = scia_dark_fun(p, x)
      
      Inputs:
      
-     x:    An n-element vector of independent variables
-           * x[0]:         orbit phase in range (can be [0..1] but also beyond for trending)
-           * x[1]:         pixel exposure time in s
-           * x[2]:         co-adding factor
+     p:    tuple containing the function parameters
+           * p[0]: numpy array, analog offset in BU for 1024 pixels
+           * p[1]: numpy array, dark current in BU/s for 1024 pixels
+           * p[2]: numpy array, orbital variation amplitude in BU/s
+           * p[3]: numpy array, linear orbit-to-orbit trend in BU/s
+           * p[4]: scalar, orbital variation first overtone relative amplitude in BU/s
+           * p[5]: scalar, orbital variation fundamental frequency phase offset in range [0-1]
+           * p[6]: scalar, orbital variation first overtone phase offset in range [0-1]
+     x:    tuple of independent variables (numpy arrays of length n_x)
+           * x[0]: numpy array, orbit phase in range (can be [0..1] but also beyond for trending)
+           * x[1]: numpy array, pixel exposure time in s
+           * x[2]: numpy array, co-adding factor
            all three input values repeated for additional spectra
-     a:    A vector containing the function parameters
-           * a[0:1023]:    analog offset in BU for 1024 pixels
-           * a[1024:2047]: dark current in BU/s for 1024 pixels
-           * a[2048:3071]: orbital variation amplitude in BU/s
-           * a[3072:4095]: linear orbit-to-orbit trend in BU/s
-           * a[4096]:      orbital variation first overtone relative amplitude in BU/s
-           * a[4097]:      orbital variation fundamental frequency phase offset in range [0-1]
-           * a[4098]:      orbital variation first overtone phase offset in range [0-1]
      
      Returns:
      f:    function values:
-           * first dimension 1024 pixel values
-           * second dimension x.size/3 spectra
+           numpy.matrix of dimensions [1024, n_x]
      
      Author:
      Pieter van der Meer, SRON, 2014
@@ -74,44 +74,37 @@ def scia_dark_fun(x, *a):
      Ralph Snel, SRON, 2008
     """
 
-    # number of pixels
-    print('x.size=',x.size)
-    print(x)
-    print('len(a)=',len(a))
-    print(a)
-    n_a = ( len(a) - 3 )/4
+    # extract parameters from p
+    analog_offset, dark_current, orb_var_amplitude1, orb_trend, orb_var_amplitude2, phase_offset1, phase_offset2 = p
+    n_a = analog_offset.size #nr of pixels
     aones = numpy.matrix(numpy.ones(n_a))
+    # transform to matrices 
+    analog_offset = numpy.matrix(analog_offset)
+    dark_current = numpy.matrix(dark_current)
+    orb_var_amp1_mat = numpy.matrix(orb_var_amplitude1)
+    orb_trend = numpy.matrix(orb_trend)
+    orb_var_amp2_mat = orb_var_amp1_mat*orb_var_amplitude2
 
-    # Extract exposure information and orbit phase from x:
-    n_x	= x.size/3
+    # Extract exposure information and orbit phase from x
+    orbit_phase, pixel_exposure_time, co_adding_factor = x
+    n_x = orbit_phase.size # nr of datapoints
     xones = numpy.matrix(numpy.ones(n_x))
 
-    orbit_phase			= x[0:-1:3]
-    pixel_exposure_time	= x[1:-1:3]
-    co_adding_factor	= x[2:-1:3]
-
-#     print('orbit_phase=', orbit_phase)
-#     print('PET=', pixel_exposure_time)
-#     print('co_adding_factor=', co_adding_factor)
-#     print('n_a=', n_a,' n_x=', n_x)
+    print('orbit_phase=', orbit_phase)
+    print('PET=', pixel_exposure_time)
+    print('co_adding_factor=', co_adding_factor)
+    print('n_a=', n_a,' n_x=', n_x)
 	
-    # Extract the fit parameters from a:
-    analog_offset		= numpy.matrix(a[0:n_a-1])
-    dark_current		= numpy.matrix(a[n_a:2*n_a-1])
-    orb_var_amplitude1	= a[2*n_a:3*n_a-1]
-    orb_trend           = numpy.matrix(a[3*n_a:4*n_a-1])
-    orb_var_amplitude2	= a[4*n_a]
-    phase_offset1       = a[4*n_a+1]
-    phase_offset2       = a[4*n_a+2]
-    orb_var_amp1_mat    = numpy.matrix(orb_var_amplitude1)
-    orb_var_amp2_mat    = numpy.matrix(orb_var_amplitude2)
-
     # Generate an array for the modeled block of detector read-outs:
     darks = numpy.zeros([n_a, n_x])
 
     #
     # Calculate the dark current:
     #
+  
+    print('dark_current.shape=',dark_current.shape)
+    print('len(orb_var_amplitude1)=',len(orb_var_amplitude1))
+    print('shapes=',darks.shape,dark_current.T.shape,xones.shape)
 
     darks += dark_current.T * xones
 
@@ -119,7 +112,7 @@ def scia_dark_fun(x, *a):
     # Fundamental frequency:
     orbital_variation1 = orb_var_amp1_mat.T * cos((orbit_phase+phase_offset1)*2*pi)
     # First overtone, amplitude relative to fundamental frequency amplitude:
-    orbital_variation2 = numpy.matrix(orb_var_amplitude1 * orb_var_amplitude2).T * cos((orbit_phase+phase_offset2)*4*pi)
+    orbital_variation2 = orb_var_amp2_mat.T * cos((orbit_phase+phase_offset2)*4*pi)
     # Total orbital variation:
     orbital_variation  = orbital_variation1 + orbital_variation2
     darks += orbital_variation
@@ -128,18 +121,33 @@ def scia_dark_fun(x, *a):
     #print('darks (no pet, no ao)=', darks)
 
     # Multiply with pixel exposure time:
-    darks *= (pixel_exposure_time * aones).T
+    print('pet.shape=',pixel_exposure_time.shape)
+    print('aones.shape=',aones.shape)
+    pet_mat = aones.T * pixel_exposure_time
+    print('pet_mat.shape=',pet_mat.shape)
+    print('darks.shape=',darks.shape)
+    darks *= pet_mat
 
     # Add the analog offset:
-    darks += (analog_offset * xones).T
+    darks += (analog_offset.T * xones)
 
     # Multiply with co-adding factor:
-    darks *= (co_adding_factor * aones).T
+    print('co_adding_factor.shape=',co_adding_factor.shape)
+    coad_mat = aones.T * numpy.matrix(co_adding_factor) 
+    darks *= coad_mat
 
-    # Reform the 2-dimensional array to a 1-dimensional vector:
-    f = darks.flatten()
+    return darks.T
 
-    return f
+def scia_dark_residuals(p, data):
+    """
+    input:
+     data: list of two: 
+     - x: independent variables (list of three equally sized numpy arrays)
+     - y: dependent variables 
+    """
+    print("BLABLA")
+    x, y = data 
+    return y - scia_dark_fun(p, x)
 
 #- main ------------------------------------------------------------------------
 
@@ -156,12 +164,13 @@ print(states['readoutMean'].shape)
 readouts = states['readoutMean']
 noise = states['readoutNoise']
 print(readouts.shape)
-for idx_exec in range(readouts.shape[0]):
+n_exec = readouts.shape[0]
+for idx_exec in range(n_exec):
     readouts[idx_exec,:] = nlc.correct(readouts[idx_exec,:])
-pet = states['pet'][0]
-coadd = states['coadd'][0]
-readouts_ = readouts[:,7*1024:8*1024] #/ coadd (was already done?)
-noise_ = noise[:,7*1024:8*1024] / numpy.sqrt(coadd)
+pet = states['pet'][0] + numpy.zeros(n_exec)
+coadd = states['coadd'][0] + numpy.zeros(n_exec)
+readouts_ = readouts[:,7*1024:8*1024] #/ coadd (was already done)
+noise_ = noise[:,7*1024:8*1024] / numpy.sqrt(coadd[0])
 state_phases = state_mtbl['orbitPhase'][:]
 
 # orbit 2
@@ -174,8 +183,8 @@ noise = states['readoutNoise']
 print(readouts.shape)
 for idx_exec in range(readouts.shape[0]):
     readouts[idx_exec,:] = nlc.correct(readouts[idx_exec,:])
-readouts2_ = readouts[:,7*1024:8*1024] #/ coadd (was already done?)
-noise2_ = noise[:,7*1024:8*1024] / numpy.sqrt(coadd)
+readouts2_ = readouts[:,7*1024:8*1024] #/ coadd (was already done)
+noise2_ = noise[:,7*1024:8*1024] / numpy.sqrt(coadd[0])
 state_phases2 = state_mtbl['orbitPhase'][:]
 
 # combine orbit data
@@ -183,15 +192,46 @@ all_state_phases = numpy.concatenate((state_phases, state_phases2))
 all_readouts = numpy.concatenate((readouts_, readouts2_)) #.flatten()
 all_sigmas = numpy.concatenate((noise_, noise2_)) #.flatten()
 
-x = numpy.empty([3, all_state_phases.size])
-x[0,:] = all_state_phases
-x[1,:] = pet
-x[2,:] = coadd
-print(x)
+#
+# fit it
+#
+
+# prepare parameters and fitter
 n_pix = 1024
-p0 = numpy.zeros(4096+3)
-print('x.shape=',x.shape)
+x = all_state_phases, pet, coadd
+print(x)
+print('len(x)=', len(x))
+print("isinstance(x,..) ", isinstance(x, numpy.ndarray))
+print("isinstance(x[0],..) ", isinstance(x[0], numpy.ndarray))
+print("isinstance(x[1],..) ", isinstance(x[1], numpy.ndarray))
+print("isinstance(x[2],..) ", isinstance(x[2], numpy.ndarray))
 print('all_readouts.shape=',all_readouts.shape)
 print('all_sigmas.shape=',all_sigmas.shape)
-params, pcov = curve_fit(scia_dark_fun, x, all_readouts, sigma=all_sigmas, p0=p0)
+fitobj = kmpfit.Fitter(residuals=scia_dark_residuals, data=(x, all_readouts))
 
+# prepare initial parameters
+ao0 = numpy.zeros(n_pix) + 4000
+dc0 = numpy.zeros(n_pix) + 4000
+amp1_0 = numpy.zeros(n_pix)
+trend0 = numpy.zeros(n_pix)
+amp2_0 = 0.1
+phase_offset1 = 0
+phase_offset2 = 0
+p0 = ao0, dc0, amp1_0, trend0, amp2_0, phase_offset1, phase_offset2
+print('len(p0)=',len(p0))
+#print('p0.size=',p0.size)
+print("isinstance(p0,..) ", isinstance(p0, numpy.ndarray))
+print("isinstance(p0[0],..) ", isinstance(p0[0], numpy.ndarray))
+print("isinstance(p0[1],..) ", isinstance(p0[1], numpy.ndarray))
+print("isinstance(p0[2],..) ", isinstance(p0[2], numpy.ndarray))
+print("isinstance(p0[3],..) ", isinstance(p0[3], numpy.ndarray))
+#print("isinstance(p0[4],..) ", isinstance(p0[4], numpy.ndarray))
+#print("isinstance(p0[5],..) ", isinstance(p0[5], numpy.ndarray))
+#print("isinstance(p0[6],..) ", isinstance(p0[6], numpy.ndarray))
+
+# ..and fit
+fitobj.fit(params0=p0)
+if (fitobj.status <= 0):
+   print('Error message = ', fitobj.message)
+else:
+   print("Optimal parameters: ", fitobj.params)
