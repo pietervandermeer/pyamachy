c79193e8f247e01fd532ccb5ecc4a8ea2495e4e4
Pieter van der Meer <pieter.van.der.meer@sron.nl>
2014-12-14 19:41:13 +0100

    added gen_vardark.sh for complete vardark db generation in both long and short varieties.

---
+++ gen_vardark.sh
@@ -0,0 +1,6 @@
+python fit_monthlies.py
+python fit_monthlies.py --short
+python interpolate_monthlies.py 
+python interpolate_monthlies.py --short
+python generate_vardark.py
+python generate_vardark.py --short
